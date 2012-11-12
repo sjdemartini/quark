c4a4c1dcfb95581c27c18a19bbb818f5b5b67853
William Li <wli@tbp.berkeley.edu>
2012-11-11 20:02:00 -0800

    Add test for quark.create_dev_db.is_valid_db_name
    Change-Id: Id381e8287544385d46aa718a3a96ae58f47dadfe

--- quark/utils/tests.py
+++ quark/utils/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+from quark.utils.create_dev_db import is_valid_db_name
 from quark.utils.dev import OFFSETS
 from quark.utils.dev import get_port
 
@@ -20,3 +21,11 @@ class DevTest(TestCase):
     def test_default_ports(self):
         self.assertEqual(get_port('', 'tbp'), 8999)
         self.assertEqual(get_port('', 'pie'), 9999)
+
+
+class CreateDevDbTest(TestCase):
+    def test_is_valid_db_name(self):
+        self.assertTrue(is_valid_db_name('quark_dev_panda'))
+
+    def test_is_valid_db_name_false(self):
+        self.assertFalse(is_valid_db_name('quark_dev_panda!'))
