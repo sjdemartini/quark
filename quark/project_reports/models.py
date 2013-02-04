2b6324d5ea11a33a1c284df60253bc34f9ccd0a5
Steven DeMartini <sjdemartini@tbp.berkeley.edu>
2013-02-04 05:38:37 -0800

    Add help_text to project report fields
    Change-Id: I3f205abc9963c531c9958732c361119914d0f800

--- quark/project_reports/models.py
+++ quark/project_reports/models.py
@@ -9,7 +9,6 @@ from quark.base.models import Term
 
 
 class ProjectReport(models.Model):
-
     PROJECT_AREA_CHOICES = (
         ('cl', 'Community/Liberal Culture'),
         ('uc', 'University/College'),
@@ -22,32 +21,40 @@ class ProjectReport(models.Model):
     title = models.CharField(max_length=80)
     author = models.ForeignKey(User)
     committee = models.ForeignKey(OfficerPosition)
-    area = models.CharField(max_length=2, choices=PROJECT_AREA_CHOICES,
-                            blank=True)
-    organize_hours = models.PositiveSmallIntegerField(default=0)
-    participate_hours = models.PositiveSmallIntegerField(default=0)
-    location = models.CharField(max_length=100)
-    is_new = models.BooleanField(default=False)
-    other_group = models.CharField(max_length=60, blank=True)
-    description = models.TextField(blank=True,
-                                   help_text=('General description of event,'
-                                              'vendors, sponsors, etc.'))
+    area = models.CharField(
+        max_length=2, choices=PROJECT_AREA_CHOICES, blank=True)
+    organize_hours = models.PositiveSmallIntegerField(
+        default=0, help_text='Number of hours spent organizing the event')
+    participate_hours = models.PositiveSmallIntegerField(
+        default=0, help_text=('Number of hours spent by a single person '
+                              'participating in the event'))
+    is_new = models.BooleanField(
+        default=False, help_text=('Was this the first time an event like this '
+                                  'had been held?'))
+    other_group = models.CharField(
+        max_length=60, blank=True,
+        help_text=('Name(s) of any other organization that also participated '
+                   'and/or helped organize the event.'))
+    description = models.TextField(
+        blank=True, help_text=('General description of event, vendors, '
+                               'sponsors, etc.'))
     purpose = models.TextField(blank=True)
-    organization = models.TextField(blank=True,
-                                    help_text=('Setup, number of people'
-                                               'involved, clean-up, etc.'))
+    organization = models.TextField(
+        blank=True, help_text=('Setup, number of people involved, clean-up, '
+                               'etc.'))
     cost = models.TextField(blank=True)
     problems = models.TextField(blank=True)
     results = models.TextField(blank=True)
-    officer_list = models.ManyToManyField(User, related_name='officer_list+',
-                                          blank=True)
-    member_list = models.ManyToManyField(User, related_name='member_list+',
-                                         blank=True)
-    candidate_list = models.ManyToManyField(User,
-                                            related_name='candidate_list+',
-                                            blank=True)
-    non_tbp = models.PositiveSmallIntegerField(default=0)
-    complete = models.BooleanField(default=False)
+    officer_list = models.ManyToManyField(
+        User, related_name='officer_list+', blank=True)
+    member_list = models.ManyToManyField(
+        User, related_name='member_list+', blank=True)
+    candidate_list = models.ManyToManyField(
+        User, related_name='candidate_list+', blank=True)
+    non_tbp = models.PositiveSmallIntegerField(
+        default=0, help_text='Number of non-TBP participants')
+    complete = models.BooleanField(
+        default=False, help_text='Is this project report finished?')
     first_completed_at = models.DateTimeField(null=True, blank=True)
     attachment = models.FileField(upload_to='pr', blank=True)
 
