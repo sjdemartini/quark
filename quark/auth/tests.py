886a97379684bbe246c034a9619adab360071f69
Nitish Padmanaban <nitishp@tbp.berkeley.edu>
2013-02-26 21:06:36 -0800

    Add tests for company user
    Change-Id: I7c83d439cb0f66d3dbc2c58b721521c5e55dd438

--- quark/auth/tests.py
+++ quark/auth/tests.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 from django.test.utils import override_settings
 
 from quark.auth.models import get_user_model
+from quark.auth.models import CompanyQuarkUser
 from quark.auth.models import LDAPQuarkUser
 from quark.auth.models import QuarkUser
 from quark.qldap.utils import username_exists
@@ -150,3 +151,50 @@ class CreateLDAPQuarkUserTestCase(CreateQuarkUserTestCase):
         self.assertTrue(username_exists(self.username))
         user.delete()
         self.assertFalse(username_exists(self.username))
+
+
+@override_settings(AUTH_USER_MODEL='auth.CompanyQuarkUser')
+class CreateCompanyQuarkUserTestCase(TestCase):
+    def setUp(self):
+        self.model = CompanyQuarkUser
+        self.username = 'testcompany'
+        self.email = 'testcompany@tbp.berkeley.edu'
+        self.password = 'testpassword'
+        self.company_name = 'Industry Company Inc.'
+
+    def test_correct_user_model(self):
+        """Make sure this test is using the proper model"""
+        self.assertEqual(self.model, CompanyQuarkUser)
+
+    def test_create_user(self):
+        """Tests create_user"""
+        self.assertFalse(
+            self.model.objects.filter(username=self.username).exists())
+        self.assertIsNotNone(self.model.objects.create_user(
+            username=self.username,
+            email=self.email,
+            password=self.password,
+            company_name=self.company_name))
+        # Make sure it's saved
+        query = self.model.objects.filter(username=self.username)
+        self.assertEqual(1, query.count())
+        user = query[0]
+        # Attributes
+        self.assertEqual(user.username, self.username)
+        self.assertEqual(user.email, self.email)
+        self.assertEqual(user.company_name, self.company_name)
+        self.assertFalse(user.is_superuser)
+        self.assertTrue(user.check_password(self.password))
+
+    def test_name_methods(self):
+        user = self.model.objects.create_user(
+            username=self.username,
+            email=self.email,
+            password=self.password,
+            company_name=self.company_name)
+
+        # Name methods
+        self.assertEqual(user.get_full_name(), self.company_name)
+        self.assertEqual(unicode(user),
+                         '{} ({})'.format(self.username, self.company_name))
+        self.assertEqual(user.get_short_name(), self.company_name)
