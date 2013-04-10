363f69cd73fa63de71636e44f4b774f2e53f8946
Steven DeMartini <sjdemartini@tbp.berkeley.edu>
2013-04-10 01:09:57 -0700

    Add base template and css for TBP
    Add base.html as the base TBP template, as well as index.html for a test
    home page. Associated CSS and images are added as well.
    
    Change-Id: Id3aac5431ae45ee571872d5851fbe8e74e52e78f

--- quark/urls.py
+++ quark/urls.py
@@ -3,10 +3,13 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic import TemplateView
+
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^courses/', include(
         'quark.courses.urls', app_name='courses', namespace='courses')),
