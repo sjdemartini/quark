f41e139ca606587daad65de575d8422dd31013dd
Steven DeMartini <sjdemartini@tbp.berkeley.edu>
2013-04-30 19:11:22 -0700

    Add more features to the Courses admin
    Change-Id: Iff21c7612bdad9d6834873148d6ae594fbd0da61

--- quark/courses/admin.py
+++ quark/courses/admin.py
@@ -6,7 +6,42 @@ from quark.courses.models import Department
 from quark.courses.models import Instructor
 
 
-admin.site.register(Course)
-admin.site.register(CourseInstance)
-admin.site.register(Department)
-admin.site.register(Instructor)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('department', 'number', 'title')
+    list_display_links = ('department', 'number')
+    list_filter = ('department',)
+    search_fields = ('number', 'title', 'department__short_name',
+                     'department__long_name', 'department__abbreviation')
+
+
+class CourseInstanceAdmin(admin.ModelAdmin):
+    list_display = ('course', 'term', 'instructor_names')
+    list_filter = ('term', 'course')
+    search_fields = ('course__number', 'course__department__short_name',
+                     'course__title', 'instructors__first_name',
+                     'instructors__last_name')
+
+    def instructor_names(self, obj):
+        return ', '.join(
+            ['{} {}'.format(instructor.first_name, instructor.last_name)
+             for instructor in obj.instructors.all()])
+    instructor_names.short_description = 'Instructor(s)'
+
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('long_name', 'short_name', 'abbreviation')
+    list_display_links = ('long_name', 'short_name', 'abbreviation')
+    search_fields = ('long_name', 'short_name', 'abbreviation')
+
+
+class InstructorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'department')
+    list_display_links = ('first_name', 'last_name')
+    list_filter = ('department',)
+    search_fields = ('first_name', 'last_name')
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(CourseInstance, CourseInstanceAdmin)
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Instructor, InstructorAdmin)
