5c5281198cd260c80ec45e53a725d555a3351bc3
Giovanni Gajudo <giovanni@tbp.berkeley.edu>
2013-05-08 23:13:23 -0700

    Add chosen plugin to project report forms
    Change-Id: I2ecf7a715ce134ccd62ed0597420bb2269da22cb

--- quark/project_reports/forms.py
+++ quark/project_reports/forms.py
@@ -1,18 +1,42 @@
+from chosen import forms as chosen_forms
 from django import forms
 from django.contrib.admin import widgets
 
+from quark.auth.models import User
+from quark.auth.fields import UserCommonNameChoiceField
+# TODO(giovanni): figure out standard for importing, example:
+#                 chosen_forms.ChosenChoiceField or just ChosenChoiceField
+from quark.auth.fields import UserCommonNameMultipleChoiceField
+from quark.base.models import Term
+from quark.base_tbp.models import OfficerPosition
 from quark.project_reports.models import ProjectReport
 
 
 class ProjectReportForm(forms.ModelForm):
-    # TODO(giovanni): Implement Chosen plugin in an __init__ method to be used
-    #                 for certain form fields: author; officer_list;
-    #                 candidate_list; member_list; semester; committee; area
-
+    committtee = chosen_forms.ChosenModelChoiceField(
+        queryset=OfficerPosition.objects.filter(
+            position_type=OfficerPosition.TBP_OFFICER))
+    area = chosen_forms.ChosenChoiceField()
+    author = UserCommonNameChoiceField(queryset=User.objects.all())
+    officer_list = UserCommonNameMultipleChoiceField(
+        queryset=User.objects.all(), required=False)
+    candidate_list = UserCommonNameMultipleChoiceField(
+        queryset=User.objects.all(), required=False)
+    member_list = UserCommonNameMultipleChoiceField(
+        queryset=User.objects.all(), required=False)
     date = forms.DateField(widget=widgets.AdminDateWidget)
-    # TODO(giovanni): Add fields similar to noiro_main.fields for the fields:
-    #                 author; officer_list; candiate_list; member_list
 
     class Meta:
         model = ProjectReport
         exclude = ('first_completed_at',)
+        widges = {
+            'term': chosen_forms.ChosenSelect(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectReportForm, self).__init__(*args, **kwargs)
+        # For fields which reference a QuerySet that must be evaluated (i.e,
+        # hits the database and isn't "lazy"), create fields in the __init__ to
+        # avoid database errors in Django's test runner
+        self.fields['term'].queryset = Term.objects.get_terms(
+            include_future=False, include_summer=True, reverse=False)
