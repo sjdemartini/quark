86ad4ae0f14d2e55d9bf67bc2089e090e0aba530
Eric Wang <ericdwang@tbp.berkeley.edu>
2013-11-09 15:19:44 -0800

    Finish migration scripts for user profiles
    * Import pictures in UserProfiles
    * Import multiple majors in CollegeStudentInfos
    * Import bios in StudentOrgUserProfiles and handle non UTF-8 encodings
    
    Change-Id: I9fb89752692e3403d162f42559239ac6eb6d9eb6

--- scripts/import_user_models.py
+++ scripts/import_user_models.py
@@ -1,8 +1,10 @@
 from dateutil import parser
 from django.contrib.auth import get_user_model
+from django.core.files import File
 from django.utils.timezone import get_current_timezone
 from django.utils.timezone import make_aware
 
+from quark.base.models import Major
 from quark.base.models import Term
 from quark.user_profiles.models import CollegeStudentInfo
 from quark.user_profiles.models import StudentOrgUserProfile
@@ -12,8 +14,17 @@ from scripts import get_json_data
 from scripts.import_base_models import SEMESTER_TO_TERM
 
 
+PICTURES_LOCATION = '/var/noiro/media/'
+# The pk's for Major start at 1 in noiro and 1000 in quark. The ordering of
+# the majors is the same, so 999 needs to be added to a noiro pk to convert it
+# to a quark pk.
+MAJOR_PK_CONVERSION = 999
+
+user_model = get_user_model()
+
+
 def delete_users():
-    users = get_user_model().objects.all()
+    users = user_model.objects.all()
     for user in users:
         user.delete()
 
@@ -23,7 +34,7 @@ def import_users():
     timezone = get_current_timezone()
     for model in models:
         fields = model['fields']
-        user = get_user_model().objects.create_user(
+        user = user_model.objects.create_user(
             pk=model['pk'],
             username=fields['username'],
             email=fields['email'])
@@ -46,10 +57,9 @@ def import_user_profiles():
     for model in models:
         fields = model['fields']
 
-        # TODO(ericdwang): transfer pictures
-        UserProfile.objects.get_or_create(
+        user_profile, _ = UserProfile.objects.get_or_create(
             pk=model['pk'],
-            user=get_user_model().objects.get(pk=fields['user']),
+            user=user_model.objects.get(pk=fields['user']),
             preferred_name=fields['preferred_name'],
             middle_name=fields['middle_name'],
             birthday=fields['birthday'],
@@ -69,25 +79,39 @@ def import_user_profiles():
             perm_state=fields['perm_state'],
             perm_zip=fields['perm_zip'],
             international_address=fields['international_address'])
+        if fields['picture']:
+            with open(PICTURES_LOCATION + fields['picture'], 'r') as picture:
+                user_profile.picture = File(picture)
+                user_profile.save()
 
-        # TODO(ericdwang): convert ManyToManyField major field in noiro to
-        # Foreignkey in quark
         student_info, _ = CollegeStudentInfo.objects.get_or_create(
             pk=model['pk'],
-            user=get_user_model().objects.get(pk=fields['user']))
-        if fields['start_semester'] is not None:
+            user=user_model.objects.get(pk=fields['user']))
+        for major_pk in fields['major']:
+            student_info.major.add(Major.objects.get(
+                pk=major_pk + MAJOR_PK_CONVERSION))
+        if fields['start_semester']:
             student_info.start_term = Term.objects.get(
                 pk=SEMESTER_TO_TERM[fields['start_semester']])
-        if fields['grad_semester'] is not None:
+        if fields['grad_semester']:
             student_info.grad_term = Term.objects.get(
                 pk=SEMESTER_TO_TERM[fields['grad_semester']])
         student_info.save()
 
-        # TODO(ericdwang): handle strange bio encodings
+        user = user_model.objects.get(pk=fields['user'])
         student_profile, _ = StudentOrgUserProfile.objects.get_or_create(
-            pk=model['pk'],
-            user=get_user_model().objects.get(pk=fields['user']))
-        if fields['initiation_semester'] is not None:
+            pk=model['pk'], user=user)
+        if fields['initiation_semester']:
             student_profile.initiation_term = Term.objects.get(
                 pk=SEMESTER_TO_TERM[fields['initiation_semester']])
         student_profile.save()
+        try:
+            # Some bios contain characters which are not in UTF-8 (the
+            # encoding for strings in the database), so try decoding the bio to
+            # check if it is formatted properly. If it's not, then decode()
+            # should raise a UnicodeDecodeError.
+            fields['bio'].decode('utf_8')
+            student_profile.bio = fields['bio']
+            student_profile.save()
+        except UnicodeEncodeError:
+            print('ERROR: Could not import {user}\'s bio.'.format(user=user))
