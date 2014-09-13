cdb42c4215c05cb3ac151df1ec7e78fbc8a597fd
Eric Yu <ehy@tbp.berkeley.edu>
2014-09-13 16:23:42 -0700

    Allow newsreel items to be edited without re-uploading the image
    https://tbp.berkeley.edu/trac/ticket/583
    
    Change-Id: I2294178f7a170525c4e2f7c007ae92a88e802741

--- quark/newsreel/forms.py
+++ quark/newsreel/forms.py
@@ -7,3 +7,8 @@ class NewsForm(forms.ModelForm):
     class Meta(object):
         model = News
         fields = ('title', 'blurb', 'image')
+
+    def __init__(self, *args, **kwargs):
+        super(NewsForm, self).__init__(*args, **kwargs)
+        if self.instance:
+            self.fields['image'].required = False
